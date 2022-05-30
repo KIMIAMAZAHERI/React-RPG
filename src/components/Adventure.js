65db9aecabddaa62197f97246ea769929a42003a
amelielidaeng <98023444+amelielidaeng@users.noreply.github.com>
2022-05-30 15:09:13 +0200

    plan adventure

--- src/components/Adventure.js
+++ src/components/Adventure.js
@@ -1,6 +1,21 @@
 //Adventure is where the game takes place - where you end up when you hit "Play"
 //Keep it simple. 
 
+//Plan is to make a simple game using the pokeapi. 
+//When user logs in they can choose an opponent and start match
+//Opponent is a random character with random attack and random everything. 
+
+//There is a grid with pokemons. 
+//User chooses a character to play so there is an onClick that sets the player
+//User then clicks button to start match (ie another onClick that opens a div with a background)
+//Button for attack (also onClick)
+//When clicking button it sets of a random attack from characters specifications
+
+//Need function for a miss as well. Might be an extra. 
+
+//Question marks: 
+//Is the health bar included in the API? 
+//Second component for the match? Arena.js ? 
 
 const Adventure = () => {
     return (
